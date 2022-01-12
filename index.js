35cdf0c74b3691199fe7571ae7673d973279dfcd
Naveen Kumar Karwsara <n29bishnoi@gmail.com>
2022-01-12 13:40:18 +0100

    theme function was added

--- index.js
+++ index.js
@@ -14,4 +14,23 @@ const formContainer = document.getElementById("form-container")
 const formSubmitBtn = document.getElementById("input-btn")
 formSubmitBtn.addEventListener("click", () => {
     formContainer.textContent = "Thank you for contact. I will get back to you soon!"
+    formContainer.classList.add("form-submit-text")
 })
+const themeToggle = document.getElementById("theme")
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("theme-open")
+    
+})
+const themeLight = document.getElementById("theme-light")
+const themeDark = document.getElementById("theme-dark")
+let root = document.querySelector(":root")
+
+
+themeDark.addEventListener("click", () => {
+    document.body.classList.add("dark-theme")
+    
+})
+themeLight.addEventListener("click", () => {
+    document.body.classList.remove("dark-theme")
+})
+
